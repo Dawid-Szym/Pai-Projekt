ed8ac22da981350952524620b922ba94a2b2b8ae
Jakub20106 <jakub20106@gmail.com>
2024-01-13 11:05:23 +0100

    Ekoncert commit

---
+++ eKoncert/Pages/Tickets.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eKoncert.Pages
+{
+    public class TicketsModel : PageModel
+    {
+        private readonly ILogger<TicketsModel> _logger;
+
+        public TicketsModel(ILogger<TicketsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
